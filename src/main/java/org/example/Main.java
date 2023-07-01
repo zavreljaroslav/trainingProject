66c431f736eac97f38096fa4f3538bbe1a1c6c93
zavreljaroslav <zavreljaroslav@email.cz>
2023-07-01 16:36:43 +0200

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,6 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner str = new Scanner(System.in);
-        String response;
-        do{
-            System.out.println("Kolik je Kaibovi?");
-            response = str.nextLine();
-            if (response.equals("15")) {
-                System.out.println("Spravna odpoved");
-            } else {
-                System.out.println("Tolik neni Kaibovi!");
-            }
-        } while (!response.equals("15"));
     }
 }
