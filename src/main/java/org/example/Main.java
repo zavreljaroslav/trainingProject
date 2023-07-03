dcaaadf38c15f72c1e34984099ec69acb80f1694
Alex <arekusis@mailfence.com>
2023-07-03 15:46:35 -0600

    Tracking number of tries + grammar!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,17 @@ import org.example.models.Person;
 
 public class Main {
     public static void main(String[] args) {
+        AgeMinigameService ageMinigameService = new AgeMinigameService();
         Person Toob = new Person();
         Toob.setAge (29);
         Toob.setName("Toob");
 
         //String userName = "Jimmi";
         //byte userAge = 25;
-        AgeMinigameService.playGame(Toob.getAge(), Toob.getName());
+        int userTries = ageMinigameService.playGame(Toob.getAge(), Toob.getName());
+
+        //grammar check for try v tries
+        ageMinigameService.grammarPolice(userTries);
+
             }
         }
--- src/main/java/org/example/service/AgeMinigameService.java
+++ src/main/java/org/example/service/AgeMinigameService.java
@@ -3,29 +3,38 @@ package org.example.service;
 import java.util.Scanner;
 
 public class AgeMinigameService {
-    //public static void playGame(String[] args) {
-    public static void playGame(int userAge, String userName) {
-        // while (!userGuess.equals("quit"))
-        //byte jimmiAge = 25;
+
+    public int playGame(int userAge, String userName) {
         Scanner inputScanner = new Scanner(System.in);
         System.out.print("How old is " + userName + "? ");
+        int numberOfTries = 0;
 
-        //System.out.print("How old is Jimmi? ");
-        //byte userGuess = inputScanner.nextByte();
-        //string userGuess = inputScanner.nextString().toLowerCase()
-        // We convert the user's input to an integer
-        // int userGuess = Integer.parseInt(userInput)
         while (true) {
 
-            byte userGuess = inputScanner.nextByte();
-            if (userGuess > userAge)
+            int userGuess = inputScanner.nextInt();
+            if (userGuess > userAge) {
                 System.out.println("Nope, " + userName + " is younger than " + userGuess + "! Guess again!");
-            else if (userGuess < userAge)
+                numberOfTries++;
+            }
+            else if (userGuess < userAge) {
                 System.out.println("Nope, " + userName + " is older than " + userGuess + "! Guess again!");
+                numberOfTries++;
+            }
             else {
                 System.out.println("Correct! " + userName +  " is " + userGuess + " years old!");
+                numberOfTries++;
                 break;
+
             }
         }
+        return numberOfTries;
+
+
+    }
+    public void grammarPolice (int numberofTries){
+        if (numberofTries == 1)
+            System.out.println("That took you " + numberofTries + " try!");
+        else
+            System.out.println("That took you " + numberofTries + " tries!");
     }
 }
