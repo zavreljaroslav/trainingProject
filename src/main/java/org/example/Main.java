223ebcc42f7c2eaf0a5fe39126628a9a30c09532
Alex <arekusis@mailfence.com>
2023-07-03 10:28:42 -0600

    Split to service Compares to Jimmi only

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,9 @@
 package org.example;
 
-import java.util.Scanner;
+import org.example.service.AgeMinigameService;
 
 public class Main {
     public static void main(String[] args) {
-        // while (!userGuess.equals("quit"))
-        byte jimmiAge = 25;
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.print("How old is Jimmi? ");
-
-        //System.out.print("How old is Jimmi? ");
-        //byte userGuess = inputScanner.nextByte();
-        //string userGuess = inputScanner.nextString().toLowerCase()
-        // We convert the user's input to an integer
-        // int userGuess = Integer.parseInt(userInput)
-        while (true) {
-
-            byte userGuess = inputScanner.nextByte();
-            if (userGuess > jimmiAge)
-                System.out.println("Nope, Jimmi is younger than " + userGuess + "! Guess again!");
-            else if (userGuess < jimmiAge)
-                System.out.println("Nope, Jimmi is older than " + userGuess + "! Guess again!");
-            else {
-                System.out.println("Correct! Jimmi is " + userGuess + " years old!");
-                break;
+        AgeMinigameService.playGame();
             }
         }
-    }
-}
--- src/main/java/org/example/service/AgeMinigameService.java
+++ src/main/java/org/example/service/AgeMinigameService.java
@@ -1,4 +1,31 @@
 package org.example.service;
 
+import java.util.Scanner;
+
 public class AgeMinigameService {
+    //public static void playGame(String[] args) {
+    public static void playGame() {
+        // while (!userGuess.equals("quit"))
+        byte jimmiAge = 25;
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.print("How old is Jimmi? ");
+
+        //System.out.print("How old is Jimmi? ");
+        //byte userGuess = inputScanner.nextByte();
+        //string userGuess = inputScanner.nextString().toLowerCase()
+        // We convert the user's input to an integer
+        // int userGuess = Integer.parseInt(userInput)
+        while (true) {
+
+            byte userGuess = inputScanner.nextByte();
+            if (userGuess > jimmiAge)
+                System.out.println("Nope, Jimmi is younger than " + userGuess + "! Guess again!");
+            else if (userGuess < jimmiAge)
+                System.out.println("Nope, Jimmi is older than " + userGuess + "! Guess again!");
+            else {
+                System.out.println("Correct! Jimmi is " + userGuess + " years old!");
+                break;
+            }
+        }
+    }
 }
